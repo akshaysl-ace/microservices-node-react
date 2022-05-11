626d9fff595c3a2dbde4538c002d1ccebebf53e8
Akshay S L <akshaysl3867@gmail.com>
2022-05-12 00:13:03 +0530

    DB connection and auth models added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from 'mongoose';
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -22,6 +24,18 @@ app.all('*', async (req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Auth Service Listening on port 3000...");
-});
+// Create a start function to start server with DB connection
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-serv:27017/auth');
+    console.log("Connected to Database for auth service...");
+  } catch (err) {
+    console.log(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Auth Service Listening on port 3000...");
+  });
+};
+
+start();

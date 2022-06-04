be216fef72d74859755028f37eb2c24ac1ce4478
akshaysl-ace <akshaysl3867@gmail.com>
2022-06-04 19:10:56 +0530

    added tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { NotFoundError } from '@asl.org.ticketing/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  }),
+);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+export { app };
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,18 +8,18 @@ const start = async () => {
   }
 
   if (!process.env.MONGO_URI) {
-    throw new Error('DB URI not found !');
+    throw new Error('DB uri not found !');
   }
 
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log('Connected to Auth-DB..');
+    console.log('Connected to Tickets-DB..');
   } catch (err) {
     console.log(err);
   }
 
   app.listen(3000, () => {
-    console.log('Auth service started successfully..');
+    console.log('Tickets service started successfully..');
   });
 };
 
